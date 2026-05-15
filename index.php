de0cbe1dc05f1540665721f473d395b029e61749
naikotichaithanya <naikotichaithanya@gmail.com>
2026-05-15 23:35:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Blog Project</title>
+</head>
+
+<body>
+
+<h1>Blog Management System</h1>
+
+<a href="signup.php">Signup</a>
+
+<br><br>
+
+<a href="login.php">Login</a>
+
+<br><br>
+
+<a href="add_post.php">Add Post</a>
+
+<br><br>
+
+<input type="text" placeholder="Search Posts">
+
+<br><br>
+
+<div>
+
+<h3>Sample Blog Post</h3>
+
+<p>This is sample content.</p>
+
+<button>Edit</button>
+
+<button>Delete</button>
+
+</div>
+
+</body>
+</html>
